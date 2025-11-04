7b05ee5ed6c6381057194687bc0608cd8d4240ad
varavrey <thertianainki@gmail.com>
2025-11-04 10:42:52 +0700

    revisi header pada category done

--- category.php
+++ category.php
@@ -10,7 +10,18 @@ include('includes/config.php');
 
 <head>
 
-    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <title>Live News Portal | Category Page</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/icons.css">
 </head>
 
 <body>
